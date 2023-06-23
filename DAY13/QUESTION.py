b5fa754a6e411d063f4c8cdde090577b08932067
pentaggram10 <kyg44112@gmail.com>
2023-06-23 23:11:17 +0900

    d

--- DAY13/QUESTION.py
+++ DAY13/QUESTION.py
@@ -1,5 +1,25 @@
-a=[0,0,0,0,0,0,0,0,0,0]
-for i in range(0,1000):
-    for n in str(i):
-        a[int(n)]+=1
-print(a)
+# a=[0,0,0,0,0,0,0,0,0,0]
+# for i in range(1,1001):
+#     charI=str(i)
+#     for n in charI:
+#         idx=int(n)
+#         a[idx]+=1
+# print(a)
+
+# arr=[-2,3,2,-5,7]
+# neg=[]
+# pos=[]
+# for i in arr:
+#     if i<0:
+#         neg.append(i)
+#     else:
+#         pos.append(i)
+# print(neg+pos)
+
+sum=0
+for i in range(1,1001):
+    if i%3==0:
+        sum+=i
+    elif i%5==0:
+        sum+=i
+print(sum)

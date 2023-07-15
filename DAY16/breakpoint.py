eba07227e333ddf13439c34ba1c6c1d3ceacbaee
pentaggram10 <kyg44112@gmail.com>
2023-07-15 10:06:26 +0900

    dd

---
+++ DAY16/breakpoint.py
@@ -0,0 +1,27 @@
+# a=10
+# print(a)
+# b=20
+# print(b)
+# c=a+b
+# print(c)
+# d=50
+# e=10
+# if d>e:
+#     print(d)
+# else:
+#     print(e)
+# for i in range(3,20,5):
+#     print(i)hy
+# for i in range(100,-101,-10):
+#     if i>20:
+#           print(i)
+#     if i<-20:
+#         print(i)
+# sum=0
+# for i in range(50,101):
+#     if i%6==0:
+#         sum+=i
+# print(sum)
+a=[3,7,2,1,5]
+a.sort()
+print(a[2])

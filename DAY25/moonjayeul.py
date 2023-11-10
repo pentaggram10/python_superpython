a0f36a3ebf62e24f7ed5fc913252d2b82d2f8f8c
pentaggram10 <kyg44112@gmail.com>
2023-11-11 00:19:42 +0900

    dd

---
+++ DAY25/moonjayeul.py
@@ -0,0 +1,63 @@
+# str="I am a boy"
+# str2="aa,bb,cc,dd"  
+# str3="www.google.co.kr"
+# idx=str.find("b")
+# print(idx)
+# idx2=str.find("c")
+# print(idx2)
+# idx3=str.find("am")
+# print(idx3)
+# p=str.split()
+# print(p)
+# p2=str2.split(",")
+# print(p2)
+# p3=str3.split(".")
+# print(p3)
+# seven="mon,tue,wed,mon,sun,fri,thu,sat,mon,tue,fri,mon"
+# seperate=seven.split(",")
+# if 'mon' in seperate:
+#     print(seperate.count('mon'))
+# if 'tue' in seperate:
+#     print(seperate.count('tue'))
+# if 'fri' in seperate:
+#     print(seperate.count('fri'))
+# mon_cnt=0
+# tue_cnt=0
+# fri_cnt=0
+# days=seven.split(",")
+# for day in days:
+#     if day=='mon':
+#         mon_cnt+=1
+#     elif day=='tue':
+#         tue_cnt+=1
+#     elif day=='fri':
+#         fri_cnt+=1
+# print(mon_cnt)
+# print(tue_cnt)
+# print(fri_cnt)
+# mon_cnt=0
+# while True:
+#     mon_idx=seven.find("mon")
+#     if mon_idx==-1:
+#         break
+#     else:
+#         mon_cnt+=1
+#         seven=seven[mon_idx+3:]
+# print(mon_cnt)
+# str1="I am "
+# str2='a boy'
+# print(str1+str2)
+# j=''.join([str1]+[str2])
+# print(j)
+# j2='//'.join([str1]+[str2])
+# print(j2)
+# str='I am a boy'
+# p=str.split()
+# str1=p[0]
+# str2=p[1]
+# str3=p[2]
+# str4=p[3]
+# j='.'.join(p)
+# print(j)
+# j2='_'.join(p)
+# print(j2)
